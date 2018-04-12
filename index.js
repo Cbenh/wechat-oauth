7cd238af53b2b72519f081dc925774b96f848860
Cbenh <camille.benhamou@gmail.com>
2018-04-12 10:51:42 +0800

    mini app auth component

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var Oauth = require("./lib/oauth");
 var OAuthComponent = require("./lib/oauthComponent");
+var MiniAppAuthComponent = require("./lib/miniAppAuthComponent");
 
 exports.OAuth = Oauth.OAuth;
 exports.OAuthComponent = OAuthComponent.OAuthComponent;
+exports.MiniAppAuthComponent = MiniAppAuthComponent.MiniAppAuthComponent;
